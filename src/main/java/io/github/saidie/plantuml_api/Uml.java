de1ee6d78ead63b19ade2f9256e963890d1494c8
Hiroshi Saito <saito.die@gmail.com>
2017-04-28 11:36:10 +0900

    Add Uml resource class

---
+++ src/main/java/io/github/saidie/plantuml_api/Uml.java
@@ -0,0 +1,26 @@
+package io.github.saidie.plantuml_api;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Uml {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Lob @Basic(fetch=FetchType.LAZY)
+    @Column(nullable = false, columnDefinition="TEXT")
+    private String content;
+}
