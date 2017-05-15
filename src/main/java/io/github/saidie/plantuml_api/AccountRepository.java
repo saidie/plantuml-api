a0311b0fda0474ff073a85ca78f63e9c2ab3431e
Hiroshi Saito <saito.die@gmail.com>
2017-05-15 22:45:40 +0900

    Add method to find account by username to the repository

--- src/main/java/io/github/saidie/plantuml_api/AccountRepository.java
+++ src/main/java/io/github/saidie/plantuml_api/AccountRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
+    public Account findByUsername(String username);
 }
