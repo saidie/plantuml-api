0f95b0bcec455fb4d588730855473f7eeddd15e1
Hiroshi Saito <saito.die@gmail.com>
2017-05-15 22:34:45 +0900

    Remove federation repository

--- src/main/java/io/github/saidie/plantuml_api/FederationRepository.java
+++
@@ -1,6 +0,0 @@
-package io.github.saidie.plantuml_api;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface FederationRepository extends PagingAndSortingRepository<Federation, IdpId> {
-}
