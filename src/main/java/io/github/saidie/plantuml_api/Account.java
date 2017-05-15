5053d6e6525fd4141e4a959ca6ca415201154ed2
Hiroshi Saito <saito.die@gmail.com>
2017-05-15 22:39:39 +0900

    Remove a dependency from account to federations

--- src/main/java/io/github/saidie/plantuml_api/Account.java
+++ src/main/java/io/github/saidie/plantuml_api/Account.java
@@ -1,12 +1,9 @@
 package io.github.saidie.plantuml_api;
 
-import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,7 +21,4 @@ public class Account {
     @NotNull
     @Column(nullable = false)
     private String username;
-
-    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
-    private List<Federation> federations;
 }
