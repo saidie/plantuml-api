4a8ffe62a035f6e5fdbd0b00b37214ef8ef7be10
Hiroshi Saito <saito.die@gmail.com>
2017-05-20 22:53:04 +0900

    Clean SecurityConfig

--- src/main/java/io/github/saidie/plantuml_api/config/SecurityConfig.java
+++ src/main/java/io/github/saidie/plantuml_api/config/SecurityConfig.java
@@ -1,29 +1,18 @@
 package io.github.saidie.plantuml_api.config;
 
-import io.github.saidie.plantuml_api.*;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    UserService userService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll().and()
-            .formLogin().loginPage("/fuck").failureUrl("/login?error")
+            .apply(new SpringSocialConfigurer())
             ;
     }
 }
